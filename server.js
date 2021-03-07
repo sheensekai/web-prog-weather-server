2b1fa65c5fe50c1918ba6b3048f38f5c5baa3a0d
sheensekai <sheensekai@ya.ru>
2021-03-07 20:01:00 +0300

    Make favourites working endpoints

--- server.js
+++ server.js
@@ -1,15 +1,72 @@
 const express = require("express");
-const app = express();
 const weatherReq = require("./weather_req");
+
+const app = express();
+app.listen(3000);
+
 const MongoClient = require("mongodb").MongoClient;
 const mongoClient = new MongoClient("mongodb://localhost:27017");
-app.listen(3000);
+const dbName = "weather";
+const collectionName = "favourites";
+let db, col, client;
+
+mongoClient.connect(function(err, dbClient){
+    db = dbClient.db(dbName);
+    col = db.collection(collectionName);
+    client = dbClient;
+});
 
 function findCityRespond(source, res) {
     const xhr = weatherReq.makeSourceWeatherRequest(source);
     weatherReq.sendWeatherRequest(xhr, function (xhr) {
             const state = weatherReq.getWeatherStateFromResponse(xhr.responseText);
             res.send(state);
+        },
+        function (xhr) {
+            res.sendStatus(404);
+        },
+        function (xhr) {
+            res.sendStatus(429);
+        });
+}
+
+function addFavourite(col, state, res, err, result) {
+    if (result) {
+        console.log("Found one --> already contained");
+        res.sendStatus(404);
+    } else {
+        console.log("Couldn't find --> adding one");
+        col.insertOne(state, function(err, result){
+            if (err) {
+                console.log(err);
+            }
+            res.send(state);
+        });
+    }
+}
+
+function deleteFavourite(col, state, res, err, result) {
+    if (result) {
+        console.log("Found one -> return it");
+        col.deleteOne(result);
+        res.sendStatus(200);
+    } else {
+        console.log("Couldn't find --> error");
+        res.sendStatus(404);
+    }
+}
+
+function favouriteCityRespond(source, res, toAdd) {
+    const xhr = weatherReq.makeSourceWeatherRequest(source);
+    weatherReq.sendWeatherRequest(xhr, function (xhr) {
+            const state = weatherReq.getWeatherStateFromResponse(xhr.responseText);
+            col.findOne({cityId: state.cityId}, function(err, result){
+                if (toAdd) {
+                    addFavourite(col, state, res, err, result, client);
+                } else {
+                    deleteFavourite(col, state, res, err, result, client);
+                }
+            });
         },
         function (xhr) {
             res.sendStatus(404);
@@ -33,11 +90,13 @@ app.get("/weather/coordinates", function(req, res){
 });
 
 app.post("/weather/favourites", function(req, res){
-    console.log("/weather/favourites POST");
-    res.send("/weather/favourites POST");
+    const cityName = req.query.cityName;
+    const source = {byCity: true, cityName: cityName};
+    favouriteCityRespond(source, res, true);
 });
 
 app.delete("/weather/favourites", function(req, res){
-    console.log("/weather/favourites DELETE");
-    res.send("/weather/favourites DELETE");
+    const cityName = req.query.cityName;
+    const source = {byCity: true, cityName: cityName};
+    favouriteCityRespond(source, res, false);
 });
