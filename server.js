6cd50563aa1d51068c5c9ea1e0994e01e10f2279
sheensekai <sheensekai@ya.ru>
2021-03-07 17:10:30 +0300

    Make all endpoints (empty)

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
-console.log("hello");
+const express = require("express");
+const app = express();
+
+const MongoClient = require("mongodb").MongoClient;
+const mongoClient = new MongoClient("mongodb://localhost:27017");
+
+app.listen(3000);
+
+app.get("/weather/city", function(req, res){
+    console.log("/weather/city GET");
+    res.send("/weather/city GET");
+});
+
+app.get("/weather/coordinates", function(req, res){
+    console.log("/weather/coordinates GET");
+    res.send("/weather/coordinates GET");
+});
+
+app.post("/weather/favourites", function(req, res){
+    console.log("/weather/favourites POST");
+    res.send("/weather/favourites POST");
+});
+
+app.delete("/weather/favourites", function(req, res){
+    console.log("/weather/favourites DELETE");
+    res.send("/weather/favourites DELETE");
+});
